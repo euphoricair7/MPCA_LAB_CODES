42276d7cc304e66d99f0c7e617f3e545f764d0ab
Shailja <shailjashaktawat04@gmail.com>
2025-01-21 14:40:27 +0530

    fixed largest_num.s

--- week2/largest_num.s
+++ week2/largest_num.s
@@ -1,19 +1,20 @@
-;Write a program to find the largest number from a given set of numbers.(Unsorted Array)
 
-.data 
-A:	.word 10,50,41,55,30,20,11,5,100
-
-.text
 LDR R0,=A
-LDR R1, [R0], #4		
-MOV R3, #8
+MOV R1,#10
+MOV R3,#0
+L1:LDR R2,[R0],#4
+   SUB R1,R1,#1
+   CMP R3,R2
+   BLT L2
+   CMP R1,#0 
+   BNE L1
+   BEQ L3
+   
+ L2: MOV R3,R2
+	 CMP R1,#0
+	 BNE L1
+	 BEQ L3
+	 
+L3: SWI 0X11
 
-L1:
-	LDR R2, [R0], #4	
-	CMP R1, R2	
-	BGT L2	
-	MOV R1, R2	
-
-L2:
-	SUBS R3, R3, #1
-	BNE L1
+A: .WORD 10,50,41,55,30,20,11,5,100,77 
